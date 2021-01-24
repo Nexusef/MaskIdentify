aa72785cfabc567cca26d7bcf5ac954829386d87
Jaden <strudjay@gmail.com>
2021-01-24 23:11:19 +0000

    Fixed bug where frames were overloading network

--- video_detector.py
+++ video_detector.py
@@ -18,48 +18,52 @@ args = vars(ap.parse_args())
 
 # Detects Mask for each frame
 def detect_mask(frame, faceNet, maskNet):
-    # Takes frame shape and passes it into the network
-    (h, w) = frame.shape[:2]
-    blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300), (104, 117, 123))
-    faceNet.setInput(blob)
-    detections = faceNet.forward()
+    try:
+        # Takes frame shape and passes it into the network
+        (h, w) = frame.shape[:2]
+        blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300), (104, 117, 123))
+        faceNet.setInput(blob)
+        detections = faceNet.forward()
 
-    # Create arrays for later
-    faces = []
-    locs = []
-    preds = []
+        # Create arrays for later
+        faces = []
+        locs = []
+        preds = []
 
-    # For every detection, extract coords and face image
-    for i in range(0, detections.shape[2]):
-        # Extract confidences=
-        confidence = detections[0, 0, i, 2]
+        # For every detection, extract coords and face image
+        for i in range(0, detections.shape[2]):
+            # Extract confidences=
+            confidence = detections[0, 0, i, 2]
 
-        # Filter out weak confidences
-        if confidence > args["confidence"]:
-            # Transforms relative box coordinates to frame coordinates
-            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-            (startX, startY, endX, endY) = box.astype("int")
+            # Filter out weak confidences
+            if confidence > args["confidence"]:
+                # Transforms relative box coordinates to frame coordinates
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
 
-            # Ensure coords are within frame
-            (startX, startY) = (max(0, startX), max(0, startY))
-            (endX, endY) = (min(w-1, endX), min(h-1, endY))
+                # Ensure coords are within frame
+                (startX, startY) = (max(0, startX), max(0, startY))
+                (endX, endY) = (min(w-1, endX), min(h-1, endY))
 
-            face = frame[startY:endY, startX:endX]
-            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-            face = cv2.resize(face, (224, 224))
-            face = img_to_array(face)
-            face = preprocess_input(face)
-            # face = np.expand_dims(face, axis=0)
+                face = frame[startY:endY, startX:endX]
+                face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+                face = cv2.resize(face, (224, 224))
+                face = img_to_array(face)
+                face = preprocess_input(face)
+                # face = np.expand_dims(face, axis=0)
 
-            faces.append(face)
-            locs.append((startX, startY, endX, endY))
+                faces.append(face)
+                locs.append((startX, startY, endX, endY))
 
-    # Only want to predict if at least one face is detected
-    if len(faces) > 0:
-        faces = np.array(faces, dtype="float32")
-        preds = maskNet.predict(faces, batch_size=32)
+        # Only want to predict if at least one face is detected
+        if len(faces) > 0:
+            faces = np.array(faces, dtype="float32")
+            preds = maskNet.predict(faces, batch_size=32)
 
-    return (locs, preds)
+        return (locs, preds)
+    
+    except:
+        return (locs, preds)
 
 # Load face detector
 print("[INFO] Loading face detector model...")
